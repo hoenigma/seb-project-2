868d7f2d039972457ae3449749500846b7699a68
Matthew <mhoenig35@gmail.com>
2024-03-06 16:15:34 +0000

    linked API to characterlist

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 
 function CharacterList(){
 
+    
+     async function fetchAmiibo() {
+      const resp = await fetch("https://www.amiiboapi.com/api/amiibo")
+      const data = await resp.json()
+      console.log(data)
+      console.log(data.amiibo[0].character)
+      console.log(data.amiibo[0].image)
+      
+    }
+    fetchAmiibo()
+
 }
 
 export default CharacterList 
