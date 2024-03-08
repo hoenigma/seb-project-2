ad79aacfc50085062f7992c71fafa1966d088dec
Matthew <mhoenig35@gmail.com>
2024-03-08 12:40:09 +0000

    gave home page a hover box for the title

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,13 +5,16 @@ function Home() {
   return (
     <>
       <section className="hero is-fullheight-with-navbar">
-        <div className="hero-body">
-          <div className="container amiibo has-text-centered" id="intro">
+        
+        <div className="hero-body ">
+          <div className="container amiibo has-text-centered " id="intro">
+            <div className ="anything">
             <Link to={"/characterlist"}>
-              <h2 className="title">Amiibo Search</h2>
-              <p className="subtitle">Details on all your favourite Amiibos!</p>
+              <h2 id="hometitle">Amiibo Search</h2>
+              <p id="homesubtitle">Click to see all your favourite Amiibos!</p>
             </Link>
           </div>
+        </div>
         </div>
       </section>
     </>
